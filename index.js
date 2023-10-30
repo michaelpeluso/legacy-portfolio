d602bf2f3ee9122aad0543bbf1ff95ec0cda72be
Michael Peluso <mp272@njit.edu>
2023-10-30 02:18:02 -0400

    update logos and add education page

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
+/*
 $(".toggleImage").click(function () {
     const isRotated = $(".toggleImage").data("rotated");
     $(".toggleImage").css("transform", isRotated ? "rotate(0deg)" : "rotate(45deg)");
     $(".toggleImage").data("rotated", !isRotated);
+});
+*/
+
+document.addEventListener("DOMContentLoaded", function () {
+    const rotateables = document.querySelectorAll(".toggleImage");
+
+    rotateables.forEach((element) => {
+        element.addEventListener("click", function () {
+            // Toggle the "rotate90" class on the clicked element
+            this.classList.toggle("rotate90");
+        });
+    });
 });
