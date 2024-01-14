f3dec36c05c137dd64a155e861ad0713a7927801
Michael Peluso <mp272@njit.edu>
2024-01-14 12:24:17 -0500

    update hero section

--- index.js
+++ index.js
@@ -1,18 +1,46 @@
-/*
-$(".toggleImage").click(function () {
-    const isRotated = $(".toggleImage").data("rotated");
-    $(".toggleImage").css("transform", isRotated ? "rotate(0deg)" : "rotate(45deg)");
-    $(".toggleImage").data("rotated", !isRotated);
-});
-*/
-
 document.addEventListener("DOMContentLoaded", function () {
-    const rotateables = document.querySelectorAll(".toggleImage");
+    /* HTML injection */
+    fetch("/templates/head.html")
+        .then((response) => response.text())
+        .then((data) => {
+            $("head").prepend(data);
+            $("nav").addClass("sticky-top");
+        });
+    fetch("/templates/nav.html")
+        .then((response) => response.text())
+        .then((data) => {
+            $("nav").html(data);
 
+            var currentFile = window.location.pathname.split("/")[1];
+            if (currentFile == "index.html") {
+                $("#nav-home").addClass("active");
+            } else {
+                $("#nav-" + currentFile).addClass("active");
+            }
+        });
+    fetch("/templates/footer.html")
+        .then((response) => response.text())
+        .then((data) => {
+            $("footer").html(data);
+        });
+
+    // Button icon rotation
+    const rotateables = document.querySelectorAll(".toggleImage");
     rotateables.forEach((element) => {
         element.addEventListener("click", function () {
             // Toggle the "rotate90" class on the clicked element
             this.classList.toggle("rotate90");
         });
+    });
+
+    // Smooth scroll to top
+    $("#scrollToTop").click(function () {
+        $("body,html").animate(
+            {
+                scrollTop: 0,
+            },
+            800
+        );
+        return false;
     });
 });
